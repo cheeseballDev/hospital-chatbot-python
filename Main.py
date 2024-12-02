a28c64d29bffef4be8b3f44a5d63f1e42082814f
cheeseballDev <cheeseballbusinessemail@gmail.com>
2024-12-02 17:13:06 +0800

    Removed duplicate codes

--- Main.py
+++ Main.py
@@ -57,39 +57,6 @@ def check_response_valid() -> str:
             print("I'm sorry, I didn't quite understand what you said.")
             continue
     
-def response_output(user_response) -> str:
-    response = []
-    for word in user_response:
-        if word in greetings:
-            response.append(response_greetings(word))
-        elif word in diagnosis:
-            response.append(response_diagnosis(word))
-        elif word in doctor:
-            response.append(response_doctor(word))
-    if response:
-        return " ".join(response)
-    return "I'm sorry, I didn't understand what you said. Can you repeat it for me?"
-
-
-def response_greetings(user_response) -> str:
-    return str(greetings[user_response])
-
-def response_diagnosis(user_response) -> str:
-    return str(diagnosis[user_response])
-
-def response_doctor(user_response) -> str:
-    return str(doctor[user_response])
-
-
-## check if user response is valid or not
-def check_response_valid():
-    while True:
-        try:
-            user_response = input().lower().strip().translate(str.maketrans('','',"!?.,'123456789")).split()
-            return user_response
-        except:
-            print("I'm sorry, I didn't quite understand what you said.")
-            continue
 
 if __name__ == "__main__":
     main()
